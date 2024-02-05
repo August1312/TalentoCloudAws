6cb86f9c5fca86b384d516115b52ed137fc82492
Danilo Silva dos Santos <danilo.silva.santos.1312@outlook.com>
2024-02-04 22:14:57 -0300

    Arrys

--- Arry.py
+++ Arry.py
@@ -1,5 +1,14 @@
 lista_frutas = ['banana', 'maçã', 'laranja', 'abacaxi', 'pera']
 lista_booleano = [False, True, True, False]
 lista_numero = [1, 2 ,3 ,4 ,5 ,6, 7, 8, 9]
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 
+                 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 
+                 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'] 
+lista_num = [2, 45, 65, 78, 126, 987, 457, 345, 679, 107, 2345, 452, 3, 34, 560]
+print(f'A arry musica tem {len(lista_musicos)} elementos nela \n')
+print(f'O index 2 e {lista_musicos[2]}')
+print(f'O index 9 e {lista_musicos[9]}')
+print(f'O index 14 e {lista_musicos[14]}')
 
-print(f'Frutas: {lista_frutas}\n  booleano{lista_booleano} \n numeros {lista_numero}')
+for i in range(1, len(lista_num) +1):
+    print(i)
